438e2d5411d14f2f4865503f1d6fb7e78529d591
hs79327 <167207729+hs79327@users.noreply.github.com>
2024-04-17 13:08:18 +0000

    container config done

---
+++ 01_basics/variables.js
@@ -0,0 +1,10 @@
+const accountId = 200898;
+let accountName = "Harshal";
+var accountEmail = "Harshal@gmail.com";
+accountCity = "Bhilai";
+
+// accountId = 125345
+
+// console.log(accountId);
+
+console.table([accountId, accountName, accountEmail, accountCity]);
