c153f150587abda3fc1747134019fd9917a46457
hs79327 <167207729+hs79327@users.noreply.github.com>
2024-04-17 13:32:18 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,7 @@
+"use strict"; // To use JS Code in newer version
+
+// alert("Hello"); We are using Node, not a browser.
+
+console.log(typeof null); // Output => object
+
+console.log(typeof undefined); // Output => undefined
