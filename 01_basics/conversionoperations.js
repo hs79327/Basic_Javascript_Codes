cffe8c0e420e30b96483450a3db808613c9d230c
hs79327 <167207729+hs79327@users.noreply.github.com>
2024-04-18 13:31:47 +0000

    coversion operations done

---
+++ 01_basics/conversionoperations.js
@@ -0,0 +1,42 @@
+let score = "1000";
+
+// console.log(typeof(score));
+// console.log(score);
+
+
+let scoreInNumber = Number(score);
+// console.log(typeof(scoreInNumber));
+// console.log(scoreInNumber);
+
+// "1000" ==> 1000
+// "harshal123" ==>  NaN  (Not a Number)
+// Null ==> 0
+// undefined ==> NaN
+// true ==> 1 or false ===> 0
+
+
+let isLoggedIn = 1;
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+
+//1 ==> true
+//"" ==> false
+//"harshal" ==> true
+
+
+let number = 10;
+
+let stringNumber = String(number);
+// console.log(stringNumber);
+// console.log(typeof(stringNumber));
+
+
+// .........................................OPERATIONS.............................................
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log(1 + "2" + 2);
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
