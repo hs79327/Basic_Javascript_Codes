5c4b373da2a328c7f27f3cfe5a66a6cecfddbdf8
hs79327 <167207729+hs79327@users.noreply.github.com>
2024-04-23 13:22:21 +0000

    comparisons done

---
+++ 01_basics/comparisons.js
@@ -0,0 +1,22 @@
+// let x = 3;
+// let y = x++;
+// console.log(`x:${x}, y:${y}`);
+// output ==>> x:4, y:3
+
+let x = 3;
+let y = ++x;
+// console.log(`x:${x}, y:${y}`); // output ==>> x:4, y:4
+
+// console.log(null > 0); //output ==> false
+// console.log(null == 0); //output ==> false
+// console.log(null >= 0); //output ==> true
+
+// console.log(undefined > 0); //output ==> false
+// console.log(undefined == 0); //output ==> false
+// console.log(undefined >= 0); //output ==> false
+
+// console.log("2" == 2); //output ==> true
+
+// ===
+
+console.log("2" === 2); //output ==> false
