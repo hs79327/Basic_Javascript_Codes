4ed94e0414f42aa8f2168a28ccd984b8c2ceceec
hs79327 <167207729+hs79327@users.noreply.github.com>
2024-04-25 13:44:06 +0000

    String done

---
+++ 01_basics/strings.js
@@ -0,0 +1,38 @@
+const name = 'harshal';
+const age = 25;
+
+// console.log(name + " " +age);
+
+// console.log(`My name is ${name} and My age is ${age}`);
+
+const newName = new String('HarshalHS');
+
+// console.log(newName[0]);
+// console.log(newName.__proto__);
+
+// console.log(newName.length);
+// console.log(newName.toUpperCase());
+// console.log(newName.charAt(2));
+// console.log(newName.indexOf('r'));
+
+// const newString = newName.substring(0,4);
+// console.log(newString);
+
+// const anotherString = newName.slice(-8,4);
+// console.log(anotherString);
+
+// const newStringOne = new String('    Harshal    ');
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+
+const url = "https://harshal.com/harshal%20sharma";
+
+// console.log(url.replace('%20', '_'));
+// console.log(url.includes('harshal'));
+
+const newStringTwo = 'Harshal-Sharma B'
+
+console.log(newStringTwo);
+console.log(newStringTwo.split('-'));
+
