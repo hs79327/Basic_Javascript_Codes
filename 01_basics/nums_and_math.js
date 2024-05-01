99dd12f72ba5a8179ed88415ce51b3d5c3081c86
hs79327 <167207729+hs79327@users.noreply.github.com>
2024-05-01 12:59:40 +0000

    nums and math done

---
+++ 01_basics/nums_and_math.js
@@ -0,0 +1,15 @@
+// const score = 500;
+// console.log(score);
+
+const balance = new Number(200);
+
+// console.log(balance.toString());
+// console.log(balance);
+
+
+const otherNum = new Number(200.800);
+// console.log(otherNum.toFixed(2));
+// console.log(otherNum.toPrecision(3));
+
+const hundreds = 1000000;
+console.log(hundreds.toLocaleString('en-IN'));
