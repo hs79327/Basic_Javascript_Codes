856f8413a752ebce5dae415f972cadceb915a4c7
hs79327 <167207729+hs79327@users.noreply.github.com>
2024-07-15 17:14:26 +0000

    num and math

--- 01_basics/nums_and_math.js
+++ 01_basics/nums_and_math.js
@@ -11,5 +11,25 @@ const otherNum = new Number(200.800);
 // console.log(otherNum.toFixed(2));
 // console.log(otherNum.toPrecision(3));
 
-const hundreds = 1000000;
-console.log(hundreds.toLocaleString('en-IN'));
+// const hundreds = 1000000;
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// ******************** Math *******************************
+
+// console.log(Math);
+// console.log(Math.abs(-5)); // -ve to +ve only
+// console.log(Math.round(4.2));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.2));
+// console.log(Math.min(20,10,30,5,4));
+// console.log(Math.max(20,10,30,5,4));
+
+
+// console.log(Math.random());
+// console.log(Math.floor(Math.random()*10) + 1);  // IMP
+
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max-min + 1)) + min);
