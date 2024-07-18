f0586a3215bbd52e5ee12e1620c4651ebd1d49a8
hs79327 <167207729+hs79327@users.noreply.github.com>
2024-07-18 17:29:27 +0000

    dates

--- 01_basics/dates.js
+++ 01_basics/dates.js
@@ -1,7 +1,23 @@
 // Dates
 
-let myDate = new Date();
-console.log(myDate.toString());
-console.log(myDate.toLocaleString());
-console.log(myDate.toJSON());
-console.log(myDate.toDateString());
+// let myDate = new Date();
+// console.log(myDate.toString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toDateString());
+
+
+// let createdDate = new Date(2025, 7 , 20);
+let createdDate = new Date("08-20-2024");
+// console.log(createdDate.toLocaleString());
+let myTimeStamp = Date.now();
+
+// console.log(myTimeStamp);
+// console.log(createdDate.getTime());
+
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date();
+console.log(newDate.toLocaleString('default',{
+    month:"2-digit"
+}));
